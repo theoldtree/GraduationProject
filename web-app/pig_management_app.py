70dd3bc35424a952b690e5a180642becf5a9ea6a
theoldtree <yjeongmu@gmail.com>
2022-08-24 16:23:02 +0900

    configuration for mysql

--- web-app/pig_management_app.py
+++ web-app/pig_management_app.py
@@ -1,5 +1,26 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import mysql.connector
 
-st.title('Hello world')
+st.title('양돈관리')
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
