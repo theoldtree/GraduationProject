7786f116dd4453f51771b29e7794e362ec7f63c1
theoldtree <yjeongmu@gmail.com>
2022-11-16 16:45:00 +0900

    code modify for mqtt

--- web-app/mysql_mqtt/led_publisher.py
+++ web-app/mysql_mqtt/led_publisher.py
@@ -1,10 +1,39 @@
+from gpiozero import Button
+from gpiozero import LED
+from datetime import datetime
 import paho.mqtt.publish as publisher
-def showMenu():
-    print("select menu")
-    print("1.led on")
-    print("2.led off")
-    print("3.system off")
-    
+
+button_red = Button(17)
+led_red = LED(22)
+button_green = Button(24)
+led_green = LED(25)
+state = 0
+
+stringdate = datetime.strftime(datetime.now())
+print(stringdate)
+
+def red_button_pressed():
+    state = 0
+    led_red.on()
+    publisher.single("iot/led",state,hostname='localhost')
+
+def red_button_released():
+    led_red.off()
+
+def green_button_released():
+    led_green.off()
+
+def green_button_pressed():
+    state = 1
+    led_green.on()
+    publisher.single("iot/led",state,hostname='localhost')
+
+while True:
+    button_red.when_pressed = red_button_pressed
+    button_green.when_released = red_button_released
+    button_green.when_pressed = green_button_pressed
+    button_green.when_released = green_button_released
+
 if __name__ == "__main__":
     while True:
         showMenu()
@@ -17,4 +46,4 @@ if __name__ == "__main__":
         else:
             exit(0)
             
-        publisher.single("iot/led",led_state,hostname = "172.1.30.71")
+        publisher.single("iot/led",state,hostname = "172.1.30.71")
